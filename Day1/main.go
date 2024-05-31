1e0cf1d5f0cc4ff31984c23c652c84e7794689c6
arsyds <arsyds@proton.me>
2024-05-31 15:36:00 +0700

    feat: easy - two sums

---
+++ Day1/main.go
@@ -0,0 +1,12 @@
+package day1
+
+func TwoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+	for k, v := range nums {
+		if i, ok := hashMap[target-v]; ok {
+			return []int{i, k}
+		}
+		hashMap[v] = k
+	}
+	return nil
+}
---
+++ Day1/main_test.go
@@ -0,0 +1,25 @@
+package day1
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	input := []struct {
+		nums   []int
+		target int
+	}{
+		{[]int{2, 7, 11, 15}, 9},
+		{[]int{3, 2, 4}, 6},
+		{[]int{3, 3}, 6},
+	}
+	expectResult := [][]int{{0, 1}, {1, 2}, {0, 1}}
+
+	for k, v := range input {
+		result := TwoSum(v.nums, v.target)
+		if reflect.DeepEqual(result, expectResult[k]) == false {
+			t.Errorf("return wrong result, expect:%v, got:%v", expectResult[k], result)
+		}
+	}
+}
